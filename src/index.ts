89654cb3df843785243985d2d509f74f176516c9
maharshigor <gor.maharshi@gmail.com>
2023-03-10 23:52:40 -0500

    first successful query execution

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import express from "express";
+import "./models/index.js";
+import { v4 as uuidv4 } from 'uuid';
+import { contact, contactCreationAttributes } from './models/init-models';
 const port = 8100;
 
 const app = express();
@@ -7,8 +10,20 @@ app.get("/", (req, res) => {
   res.send("HELLO FROM EXPRESS");
 });
 
-app.get("/hi", (req, res) => {
+app.get("/hi", async (req, res) => {
+  try {
+    const contactValue: contactCreationAttributes = {
+      id: uuidv4(),
+      name: 'Maharshi Gor',
+      mobile: '8128376007',
+    } 
+    await contact.create(contactValue)
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
   res.send("HELLO FROM EXPRESS!!");
+
 });
 
 app.listen(port, () => {
--- src/models/index.js
+++ src/models/index.js
@@ -7,6 +7,7 @@ const process = require("process");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.js")[env];
+const { initModels } = require("./init-models");
 const db = {};
 
 let sequelize;
@@ -21,28 +22,7 @@ if (config.use_env_variable) {
   );
 }
 
-fs.readdirSync(__dirname)
-  .filter((file) => {
-    return (
-      file.indexOf(".") !== 0 &&
-      file !== basename &&
-      file.slice(-3) === ".ts" &&
-      file.indexOf(".test.js") === -1
-    );
-  })
-  .forEach((file) => {
-    const model = require(path.join(__dirname, file))(
-      sequelize,
-      Sequelize.DataTypes
-    );
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
+initModels(sequelize);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
