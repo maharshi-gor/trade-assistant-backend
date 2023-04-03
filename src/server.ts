a6fb320d874a9bda212085ea7d72ed6f6c612020
maharshigor <gor.maharshi@gmail.com>
2023-04-03 15:50:07 -0400

    first api working

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { Request } from "express";
+import cors, { CorsOptions } from "cors";
 
 // Initializing database
 import "./models/index.js";
@@ -9,6 +10,20 @@ import authenticated from "./routes/authenticated";
 const app = express();
 
 app.use(express.json());
+
+const allowlist = ["http://localhost:5173"];
+
+const corsOptionsDelegate = (req: Request, callback: Function) => {
+  var corsOptions;
+  if (req.header("Origin") && allowlist.indexOf(req.header("Origin")!) !== -1) {
+    corsOptions = { origin: true };
+  } else {
+    corsOptions = { origin: false };
+  }
+  callback(null, corsOptions);
+};
+
+app.use(cors(corsOptionsDelegate));
 app.use("/api", authenticated);
 
 export default app;
